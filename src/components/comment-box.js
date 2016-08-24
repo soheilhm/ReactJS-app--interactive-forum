23c25b5380a47831ffbb5a1f9bf6381a261c0a45
“soheilhm” <soheil.hm@gmail.com>
2016-08-24 13:20:55 -0700

    added hash history and apiUrl

--- src/components/comment-box.js
+++ src/components/comment-box.js
@@ -94,7 +94,7 @@ export default class CommentBox extends React.Component {
   _fetchComments() {
     jQuery.ajax({
       method: 'GET',
-      url: 'comments.json',
+      url: this.props.apiUrl,
       success: (comments) => {
         this.setState({ comments })
       }
@@ -108,4 +108,9 @@ export default class CommentBox extends React.Component {
 
     this.setState({ comments });
   }
+}
+
+
+CommentBox.propTypes = {
+  apiUrl: React.PropTypes.string.isRequired
 }
